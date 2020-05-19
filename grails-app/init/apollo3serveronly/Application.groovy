a4e16abae772f8775958a35bcdf57c8149c688cd
Nathan Dunn <ndunn@me.com>
2020-05-19 15:33:56 -0700

    reverted

--- grails-app/init/apollo3serveronly/Application.groovy
+++ grails-app/init/apollo3serveronly/Application.groovy
@@ -4,19 +4,19 @@ import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
 import groovy.transform.CompileStatic
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Import
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
-import springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Import
+//import springfox.documentation.spring.web.plugins.Docket
+//import springfox.documentation.swagger2.annotations.EnableSwagger2
+//import springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration
 
 
 // 1. Enable SpringFox on your project
-@EnableSwagger2
+//@EnableSwagger2
 //// 2. Import the springfox grails integration configuration
 //@Import([springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration])
-@Import([SpringfoxGrailsIntegrationConfiguration])
-//@CompileStatic
+//@Import([SpringfoxGrailsIntegrationConfiguration])
+@CompileStatic
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
